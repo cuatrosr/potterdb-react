5929b1b2c0cc76f02db1757de911091721af5ec2
G20-00 <sb5021087@gmail.com>
2023-09-30 00:33:27 -0500

    feat: add custom font and mui theme

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+let theme = createTheme({
+  typography: {
+    fontFamily: ['Montserrat', 'sans-serif'].join(","),
+  },
+});
+theme = responsiveFontSizes(theme);
+
+export default theme;
